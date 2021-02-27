ce574baeb33e7b97014b1caa04c09477e73e4451
W7k-J <59022102+W7k-J@users.noreply.github.com>
2021-02-27 16:52:45 +0100

    Update main.py
    stage 3

--- main.py
+++ main.py
@@ -6,18 +6,14 @@ Created on Thu Feb 18 11:21:52 2021
 """
 import math
 
-
-# loan_principal = int(input("Enter the loan principal"))
-
-
 def nominal_intrest_rate(percent):
     return ((percent * 1/100) / 12 * 1)
     
 def months_to_years(months):
     if months % 12 == 0:
-        return months // 12
+        return months // 12, 0 
     else:
-        return (months - (months % 12)) / 12 , months % 12
+        return int((months - (months % 12)) / 12) , int(months % 12)
 
     
 while True:
@@ -27,46 +23,50 @@ while True:
     type "p" for loan principal:""")
     decision = input()
     if decision == "n":
-        loan_principal = int(input("Enter the loan principal"))
-        monthly_payment = int(input("Enter the monthly payment:"))
-        interest_percent = int(input("Enter the loan interest:"))
+        loan_principal = float(input("Enter the loan principal"))
+        monthly_payment = float(input("Enter the monthly payment:"))
+        interest_percent = float(input("Enter the loan interest:"))     
         
         intrest = nominal_intrest_rate(interest_percent)
-        print(intrest)
-        months = (math.log1p(intrest) * (monthly_payment / (monthly_payment - intrest * loan_principal)))
+        months = math.ceil(math.log(float(monthly_payment) / (float(monthly_payment) - intrest * float(loan_principal)), intrest + 1))
+
+        y, m = months_to_years(months)
+        if m == 0:
+            if y == 0:
+                print("wrong data ")
+                continue
+            if y == 1:
+                print("It will take 1 year to repay this loan!")             
+            if y > 1:
+                print(f"It will take {y} years to repay this loan!")
+        else:
+            if y == 0:
+                print(f"It will {m} months to repay this loan!")
+            else:
+                print(f"It will take {y} years and {m} months to repay this loan!")
+        break
+    
+    if decision == "a":
+        loan_principal = float(input("Enter the loan principal"))
+        months = float(input("Enter the number of periods:"))
+        interest_percent = float(input("Enter the loan interest:"))
         
-        print(months)
-        print("--------------")
-        # It will take 8 years and 2 months to repay this loan!
-        
-        
-        
-        # if loan_principal % m == 0:
-        #     p = loan_principal / m
-        #     print(f"Your monthly payment = {p}")
-        #     break
-        # else:
-        #     p = math.ceil(loan_principal / m)
-        #     p_last = loan_principal - (p * (m - 1))
-        #     print(f"Your monthly payment = {p} and the last payment = {p_last}")
-        #     break
+        intrest = nominal_intrest_rate(interest_percent)
+        monthly_payment = math.ceil(loan_principal * ((intrest * math.pow(1 + intrest, months)) / (math.pow(1 + intrest, months) - 1)))
+
+        print(f"Your monthly payment ={monthly_payment}!")
+        break
+      
     elif decision == "p":
-        loan_principal = int(input("Enter the loan principal"))
-        p = int(input("Enter the monthly payment:"))
-        if loan_principal <= p:
-            print("It will take 1 month to repay the loan")
-            break
-        elif loan_principal % p == 0:
-            m = int(loan_principal / p)
-            print(f"It will take {m} months to repay the loan")
-            break
-        elif loan_principal % p != 0:
-            m = math.floor(loan_principal / p) + 1
-            print(m)
-            p_last = loan_principal - (p * (m - 1))
-            print(p_last)
-            print(f"It will take {m} months to repay the loan and the last payment = {p_last}")
-            break
+        annuity_payment = float(input("Enter the annuity payment"))
+        months = int(input("Enter the number of periods:"))
+        interest_percent = float(input("Enter the loan interest:"))
+        
+        intrest = nominal_intrest_rate(interest_percent)
+        loan_principal = round(annuity_payment / ((intrest * math.pow(1 + intrest, months)) / (math.pow(1 + intrest, months) - 1)))
+        
+        print(f"Our loan principal = {loan_principal}!")
+        break
     else:
         continue
 
